558565733fa01fd5bd5e6ac6eb184b0181eb8238
kevenbotelho <kral7970l@gmail.com>
2024-11-17 19:16:30 -0300

    teste de funcionamento em php

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=person" />
+    <link rel="stylesheet" href="style/reset.css">
+    <link rel="stylesheet" href="style/style.css">
+    <title>Portifólio</title>
+</head>
+<body>
+    <header>
+        <div id="conta">
+            <span class="material-symbols-outlined">
+                person
+                </span>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="">Projetos</a></li>
+                <li><a href="">Message</a></li>
+                <li><a href="">Sobre mim</a></li>
+            </ul>
+        </nav>
+        <div id="botaodelogin">
+            <button id="login">
+                Log in
+            </button>
+            <button id="signup">
+                Sign Up
+            </button>
+        
+    </header>
+    <main>
+        <section id="primeira-parte">
+            <div class="esquerdo">
+                <p class="saudacao">Olá, eu sou</p>
+                <p id="nome">Keven Botelho</p>
+                <p class="saudacao">Desenvolvedor Full-Stack</p>
+            </div>
+            <div class="direito">
+                <div class="img">
+                    <img src="https://png.pngtree.com/png-clipart/20240629/original/pngtree-an-icon-representing-a-human-face-has-entered-frame-on-a-png-image_15432531.png" alt="">
+                </div>
+            </div>
+        </section>
+        <section id="segunda_parte">
+            <div style="padding: 10px; width: 50dvw;">
+                <h2 style="padding: 10px 0;">Projetos Acadêmicos</h2>
+                <p>Apresento-vos os projetos desenvolvidos durante minha trajetória acadêmica. </p>
+            </div>
+            <div class="img" style="max-width: 200px !important;"><img src="imagens/projetos_academicos.png" alt=""></div>
+        </section>
+        <!--<section id="projetos">
+            <div class="projetos">
+                <div class="img">
+                    <img src="https://symbl-world.akamaized.net/i/webp/0c/f0c7fd9dcb878d4ac26424608addd1.webp" alt="">
+                </div>
+                <h3>ClassRecap</h3>
+            </div>
+        </section>-->
+    </main>
+</body>
+</html>
